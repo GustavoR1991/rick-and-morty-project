316383c48c42967417012fdfa9b3ff4e8f3a3aaa
GustavoR1991 <gugarosa110901@gmail.com>
2025-06-30 17:18:35 -0300

    full episode list

---
+++ components/SeeDocumentDetails/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    :to="actionUrl"
+    class="self-end bg-[#11B0C8] flex gap-2 py-[6px] pl-1 pr-2 rounded-[32px] items-center text-sm"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <IconsInfo />
+    Saiba Mais
+  </NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps({
+  actionUrl: String,
+});
+</script>
